ea9ebd12f838e8ed31c0732876df47a853decb7c
temptado <jason1287712@gmail.com>
2021-11-26 17:10:36 -0500

    got server started with module imports and exports

---
+++ server/config/connection.js
@@ -0,0 +1,12 @@
+// const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/comicSpaceDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false
+});
+
+// module.exports = mongoose.connection;
+export default mongoose.connection;
