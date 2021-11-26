2ce315579774ceab77b3c1eab6a66c8bd6755f45
temptado <jason1287712@gmail.com>
2021-11-26 17:11:46 -0500

    added database name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 // const mongoose = require('mongoose');
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/comicSpaceDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/chataholic', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true,
