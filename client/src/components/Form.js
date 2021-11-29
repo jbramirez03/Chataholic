abe668ab8aa3e104d30a7061b937c784bf4b6042
temptado <jason1287712@gmail.com>
2021-11-28 20:47:48 -0500

    tracking value of form input with state hook in react

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Form = () => {
+    const [message, setMessage] = React.useState('');
     return (
         <div>
             <form action="submit">
-                <input type="text" placeholder='message' />
+                <input type="text" placeholder='message' value={message} onChange={e => setMessage(e.target.value)} />
                 <button action='submit'>Send</button>
             </form>
         </div>
