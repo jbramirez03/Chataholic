77b699ab89d9a8698a1fda90224ad5f925a4051d
temptado <jason1287712@gmail.com>
2021-11-28 20:25:28 -0500

    added form into app.js ;

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Form from './components/Form';
+
 
 function App() {
   return (
     <div>
       <p>hello world!</p>
+      <Form />
     </div>
   );
 }
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Form = () => {
     return (
         <div>
-
+            <form action="submit">
+                <input type="text" placeholder='message' />
+                <button action='submit'>Send</button>
+            </form>
         </div>
     )
 }
