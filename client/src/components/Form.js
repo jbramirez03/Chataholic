01919ea0d61d147d3bbda6d95c065c8aa5b1634f
temptado <jason1287712@gmail.com>
2021-11-29 21:23:14 -0500

    added mutations and queries files in utils folder for client side querying

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useMutation } from '@apollo/client';
 
 const Form = () => {
     const [message, setMessage] = React.useState('');
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(message);
+    };
+
     return (
         <div>
-            <form action="submit">
+            <form action="submit" onSubmit={onSubmit}>
                 <input type="text" placeholder='message' value={message} onChange={e => setMessage(e.target.value)} />
                 <button action='submit'>Send</button>
             </form>
