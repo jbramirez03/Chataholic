efde2ddaaef40f9ae941f96e986b5a1bc9d47002
temptado <jason1287712@gmail.com>
2021-12-01 20:59:46 -0500

    got mutation for posting message to work in frontend

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { useMutation } from '@apollo/client';
+import { POST_MESSAGE } from '../utils/mutations';
 
 const Form = () => {
     const [message, setMessage] = React.useState('');
+    const [newPost] = useMutation(POST_MESSAGE);
+
+    const handlePost = async () => {
+        await newPost({
+            variables: { message: message, author: 'anonymous' }
+        })
+    };
 
     const onSubmit = (event) => {
         event.preventDefault();
+        handlePost();
         console.log(message);
     };
 
